7ec3fabbbeac82de18fac59c3805f15b0dba1490
safuvan <contactmesafwan@gmail.com>
2024-06-08 15:52:56 +0530

    static edited

--- techify/settings.py
+++ techify/settings.py
@@ -120,12 +120,11 @@ USE_I18N = True
 USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
-STATIC_URL = "static/"
-STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+STATIC_URL = "/static/"
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
-# Media Files
-MEDIA_URL = "media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+MEDIA_URL = "/media/"
 
 # Default primary key field type
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
