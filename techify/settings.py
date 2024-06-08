c3d96adf2f6aa3a54d681f5446ceaea248dcd4f0
safuvan <contactmesafwan@gmail.com>
2024-06-09 00:28:40 +0530

    requirements file updated

--- techify/settings.py
+++ techify/settings.py
@@ -121,10 +121,12 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 STATIC_URL = "/static/"
-STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static/")]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles/')
 
-MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+# Media Files
 MEDIA_URL = "/media/"
+MEDIA_ROOT = os.path.join(BASE_DIR, "media/")
 
 # Default primary key field type
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
